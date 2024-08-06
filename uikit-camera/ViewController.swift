e8778b9543b1c95ef10ec9e62602ea49583140bd
vinchen <vinchen996@gmail.com>
2024-08-06 15:20:38 +0700

    add camera session

--- uikit-camera/ViewController.swift
+++ uikit-camera/ViewController.swift
@@ -4,16 +4,73 @@
 //
 //  Created by vin chen on 02/08/24.
 //
-
+import AVFoundation
 import UIKit
 
 class ViewController: UIViewController {
 
+    var session: AVCaptureSession?
+    let previewLayer = AVCaptureVideoPreviewLayer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        view.layer.addSublayer(previewLayer)
+        checkCameraPermissions()
     }
+    
+    private func checkCameraPermissions() {
+        switch AVCaptureDevice.authorizationStatus(for: .video) {
+        case .notDetermined:
+            AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+                guard granted else {
+                    return
+                }
+                DispatchQueue.main.async {
+                    self?.setUpCamera()
+                }
+            }
+        case .restricted, .denied:
+            break
+        case .authorized:
+            setUpCamera()
+        @unknown default:
+            break
+        }
+    }
+    
+    private func setUpCamera() {
+        let session = AVCaptureSession()
 
+        guard let device = AVCaptureDevice.default(for: .video) else {
+            return
+        }
 
+        do {
+            let input = try AVCaptureDeviceInput(device: device)
+            
+            if session.canAddInput(input) {
+                session.addInput(input)
+            }
+            
+            let output = AVCaptureVideoDataOutput()
+            if session.canAddOutput(output) {
+                session.addOutput(output)
+            }
+
+            previewLayer.session = session
+            previewLayer.videoGravity = .resizeAspectFill
+            previewLayer.frame = view.layer.bounds
+            view.layer.addSublayer(previewLayer)
+            
+            DispatchQueue.global(qos: .userInitiated).async {
+                session.startRunning()
+            }
+            self.session = session
+            
+        } catch {
+            print("Error setting up camera: \(error)")
+        }
+    }
 }
 
